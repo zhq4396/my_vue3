e185d6a14d0b57bf98be5d84ffd8a496d37411d9
zhanghuaqiang <1055176305@qq.com>
2021-10-22 15:56:02 +0800

    .

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,30 +1,19 @@
 <template>
-<div class=''></div>
+  <div class="login">
+    <van-cell-group>
+      <van-cell :title="msg" value="内容" />
+      <van-cell :title="dd.name" value="内容" label="描述信息" />
+    </van-cell-group>
+    <van-button @click="sd">22</van-button>
+  </div>
 </template>
 
-<script>
-
-export default {
-name:'',
-components: {},
-data() {
-return {
-
-};
-},
-computed: {},
-watch: {},
-methods: {
-
-},
-created() {
-
-},
-mounted() {
-
-},
-}
+<script setup>
+import {  ref,reactive,toRefs } from 'vue'
+const msg = ref('Hello')
+const dd = reactive({name:'111',sex:''})
+const sd = ()=> dd.name='22'
 </script>
-<style scoped>
+<style scoped lang="less">
 
 </style>
---
+++ vue.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+    //  基本路径
+    publicPath: "./",
+    //  构建时的输出目录
+    outputDir: "vue3",
+    //  放置静态资源的目录
+    assetsDir: "static",
+    //  html 的输出路径
+    indexPath: "index.html",
+    css: {
+        loaderOptions: {
+            css: {},
+            postcss: {
+                plugins: [
+                    require('postcss-px2rem')({
+                        remUnit: 37.5 //按照屏幕宽度375px去书写页面(它可以适配vant,mintui的样式)
+                    })
+                ]
+            }
+        }
+    },
+    devServer: {
+        host: '0.0.0.0',
+        port: 3000,
+        https: false,
+        hotOnly: false,
+        proxy: {
+            '/api': {
+                target: "http://app.rmsdmedia.com",
+                changeOrigin: true,
+                secure: false,
+                pathRewrite: {
+                    "^/api": ""
+                }
+            },
+        }
+
+    }
+}
